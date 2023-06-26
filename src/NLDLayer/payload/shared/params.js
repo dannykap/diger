5c2509d07e673f051a1e6f660322eeb2b9c128f5
Danny Kaplun <dannykaplun@gmail.com>
2023-06-26 14:24:30 +0300

    remove shared

--- src/NLDLayer/payload/shared/params.js
+++
@@ -1,7 +0,0 @@
-exports.params = {
-  SERVICE_NAME: "SERVICE_NAME",
-  DYNAMO_TABLE_RESOURCE_NAME: 'mirrorsCachingTable',
-  TEMPLATE_PATH: './template.yml',
-  // replace if path to lambda code in the service's package is not as stated in template.yml (example: './dist/')
-  CODE_URI: ''  
-};
